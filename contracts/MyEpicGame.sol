80d4fc0f38b359982811dcf18457618bd70154b2
proyceus <proyceus@gmail.com>
2021-11-11 14:45:41 -0500

    finished basic implementation of smart contract

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -5,7 +5,41 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract MyEpicGame {
-    constructor() {
-        console.log("THIS IS MY GAME CONTRACT, NICE!");
+    //hold the character's attributes in a struct
+    struct CharacterAttributes {
+        uint characterIndex;
+        string name;
+        string imageURI;
+        uint hp;
+        uint maxHp;
+        uint attackDamage;
+    }
+
+    //create array to hold default data for our characters
+    CharacterAttributes[] defaultCharacters;
+
+    //data passed in to contract when first initialized
+    constructor(
+        string[] memory characterNames,
+        string[] memory characterImageURIs,
+        uint[] memory characterHp,
+        uint[] memory characterAttackDmg
+    )
+    
+    {
+        //loop through characters and save values to use later when NFTs are minted
+        for (uint i = 0; i < characterNames.length; i += 1) {
+            defaultCharacters.push(CharacterAttributes({
+                characterIndex: i,
+                name: characterNames[i],
+                imageURI: characterImageURIs[i],
+                hp: characterHp[i],
+                maxHp: characterHp[i],
+                attackDamage: characterAttackDmg[i]
+            }));
+
+            CharacterAttributes memory c = defaultCharacters[i];
+            console.log("Done initializing %s w/ HP %s, img %s", c.name, c.hp, c.imageURI);
+        }
     }
 }
