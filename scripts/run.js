306772a9a1a4b27a7894b20854601541fda59aa0
proyceus <proyceus@gmail.com>
2021-11-11 15:58:15 -0500

    added in script for NFT minting

--- scripts/run.js
+++ scripts/run.js
@@ -1,9 +1,25 @@
 const main = async () => {
     const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
-    const gameContract = await gameContractFactory.deploy();
+    const gameContract = await gameContractFactory.deploy(
+        ["Rock", "Paper", "Scissor"], //Names
+        ["https://i.imgur.com/vxlMoGz.png", "https://i.imgur.com/5rE9zeh.png", "https://i.imgur.com/zZSMKK6.png"], //images
+        [200, 300, 75], //hp values
+        [50, 25, 150] //atk values
+    );
     await gameContract.deployed();
     console.log("Contract deployed to:", gameContract.address);
+
+    let txn;
+
+    txn = await gameContract.mintCharacterNFT(2);
+    await txn.wait();
+
+    //get value of NFTs URI
+    let returnedTokenUri = await gameContract.tokenURI(1);
+    console.log("Token URI:", returnedTokenUri);
 };
+
+
 
 const runMain = async () => {
     try {
