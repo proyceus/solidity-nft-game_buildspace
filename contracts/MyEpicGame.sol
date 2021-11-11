7f508331b16f8c3e650c68e2d3cff775e7cca1b4
proyceus <proyceus@gmail.com>
2021-11-11 15:58:04 -0500

    added in NFT minting functionality

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -2,9 +2,16 @@
 
 pragma solidity ^0.8.0;
 
+//NFT contract to inherit from
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+//helper functions from OpenZeppelin
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
 import "hardhat/console.sol";
 
-contract MyEpicGame {
+contract MyEpicGame is ERC721 {
     //hold the character's attributes in a struct
     struct CharacterAttributes {
         uint characterIndex;
@@ -15,8 +22,18 @@ contract MyEpicGame {
         uint attackDamage;
     }
 
+    //The tokenId is the NFTs unique identifier
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
     //create array to hold default data for our characters
     CharacterAttributes[] defaultCharacters;
+
+    //create a mapping from the NFT's tokenId => that NFTs attributes
+    mapping(uint256 => CharacterAttributes) public nftHolderAttributes;
+
+    //create a mapping to track the address and tokenId
+    mapping(address => uint256) public nftHolders;
 
     //data passed in to contract when first initialized
     constructor(
@@ -25,7 +42,7 @@ contract MyEpicGame {
         uint[] memory characterHp,
         uint[] memory characterAttackDmg
     )
-    
+      ERC721("RoxPapesSciss", "RPS")
     {
         //loop through characters and save values to use later when NFTs are minted
         for (uint i = 0; i < characterNames.length; i += 1) {
@@ -41,5 +58,34 @@ contract MyEpicGame {
             CharacterAttributes memory c = defaultCharacters[i];
             console.log("Done initializing %s w/ HP %s, img %s", c.name, c.hp, c.imageURI);
         }
+
+        //increment tokenIds
+        _tokenIds.increment();
+    }
+
+    //users will be able to mint NFT based on the characterId they send in
+    function mintCharacterNFT(uint _characterIndex) external {
+        //get current tokenId
+        uint256 newItemId = _tokenIds.current();
+
+        //assigns tokenId to the caller's wallet address
+        _safeMint(msg.sender, newItemId);
+
+        //map tokenId to the character attributes
+        nftHolderAttributes[newItemId] = CharacterAttributes({
+            characterIndex: _characterIndex,
+            name: defaultCharacters[_characterIndex].name,
+            imageURI: defaultCharacters[_characterIndex].imageURI,
+            hp: defaultCharacters[_characterIndex].hp,
+            maxHp: defaultCharacters[_characterIndex].maxHp,
+            attackDamage: defaultCharacters[_characterIndex].attackDamage
+        });
+
+        console.log("Minted NFT w/ tokenId %s and characterIndex %s", newItemId, _characterIndex);
+
+        //keep an easy way to see who owns what NFT
+        nftHolders[msg.sender] = newItemId;
+
+        _tokenIds.increment();
     }
 }
